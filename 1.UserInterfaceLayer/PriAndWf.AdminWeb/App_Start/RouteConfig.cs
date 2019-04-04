379d47ef6b404483f2e3c519f5cf9544d5dc7e50
secretmadonna <secretmadonna@163.com>
2019-04-04 17:46:12 +0800

    完善测试

--- 1.UserInterfaceLayer/PriAndWf.AdminWeb/App_Start/RouteConfig.cs
+++ 1.UserInterfaceLayer/PriAndWf.AdminWeb/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace PriAndWf.AdminWeb
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
