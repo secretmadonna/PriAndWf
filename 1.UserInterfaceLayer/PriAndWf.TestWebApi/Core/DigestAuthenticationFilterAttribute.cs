89caa2ba35dec26fa3f358292c6cde838fcdcd44
secretmadonna <secretmadonna@163.com>
2019-10-28 17:27:09 +0800

    for pull

--- 1.UserInterfaceLayer/PriAndWf.TestWebApi/Core/DigestAuthenticationFilterAttribute.cs
+++ 1.UserInterfaceLayer/PriAndWf.TestWebApi/Core/DigestAuthenticationFilterAttribute.cs
@@ -1,100 +1,7 @@
-﻿//using log4net;
-//using PriAndWf.Application;
-//using PriAndWf.Infrastructure.Extension;
-//using PriAndWf.Infrastructure.Helper;
-//using System;
-//using System.Collections.Generic;
-//using System.Diagnostics;
-//using System.Net;
-//using System.Net.Http;
-//using System.Net.Http.Headers;
-//using System.Reflection;
-//using System.Security.Principal;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using System.Web.Http.Filters;
-//using System.Web.Http.Results;
-
-//namespace PriAndWf.TestWebApi.Core
-//{
-//    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-//    public class DigestAuthenticationFilterAttribute : FilterAttribute, IAuthenticationFilter, IFilter
-//    {
-//        private const string AuthenticationScheme = "Basic";
-//        private const string AuthenticationRealm = "realm";
-
-//        private ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-//        private UserApplicationService userApplicationService;
-
-//        public DigestAuthenticationFilterAttribute()
-//        {
-//            userApplicationService = new UserApplicationService();
-//        }
-//        public Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
-//        {
-//            var method = (MethodInfo)MethodBase.GetCurrentMethod();
-//            logger.Info(method.DescInfo() + Environment.NewLine);
-//            //var stackTrace = new StackTrace(true);
-//            //logger.Info(Environment.NewLine + stackTrace.DescInfo() + Environment.NewLine);
-
-//            return Task.Factory.StartNew(() =>
-//            {
-//                var request = context.Request;
-//                var authorization = request.Headers.Authorization;
-//                if (authorization != null)
-//                {
-//                    if (authorization.Scheme == "Digest")
-//                    {
-//                        //"Basic Base64(loginName:password)"
-//                        var loginNameAndPasswordStr = Encoding.Default.GetString(Convert.FromBase64String(authorization.Parameter));
-//                        var loginNameAndPasswordArray = loginNameAndPasswordStr.Split(new char[] { ':' }, 2, StringSplitOptions.RemoveEmptyEntries);
-//                        if (loginNameAndPasswordArray.Length == 2)
-//                        {
-//                            //登录名和密码：区分大小写？？？
-//                            var loginName = loginNameAndPasswordArray[0];
-//                            var password = loginNameAndPasswordArray[1];
-//                            var user = userApplicationService.GetByLoginName(loginName);
-//                            if (user != null && user.Password.Equals(password))
-//                            {
-//                                var principal = new GenericPrincipal(new GenericIdentity(user.UserName, "Basic"), null);
-//                                context.Principal = principal;
-//                                Thread.CurrentPrincipal = principal;
-//                            }
-//                        }
-//                        else
-//                        {
-//                            //context.ErrorResult = Unauthorized(context.Request);
-//                        }
-//                    }
-//                }
-//            });
-//        }
-//        public Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
-//        {
-//            var method = (MethodInfo)MethodBase.GetCurrentMethod();
-//            logger.Info(method.DescInfo() + Environment.NewLine);
-
-//            return Task.Factory.StartNew(() =>
-//            {
-//                var principal = context.ActionContext.ControllerContext.RequestContext.Principal;
-//                if (principal == null || !principal.Identity.IsAuthenticated)
-//                {
-//                    var challenges = new List<AuthenticationHeaderValue>();
-//                    challenges.Add(new AuthenticationHeaderValue("Digest", string.Format("realm={0}", context.Request.RequestUri.DnsSafeHost)));
-//                    context.Result = new UnauthorizedResult(challenges, context.Request);
-//                }
-//            });
-//        }
-//        public override bool AllowMultiple
-//        {
-//            get { return true; }
-//        }
-//    }
-//}
-using log4net;
+﻿using log4net;
 using PriAndWf.Application;
 using PriAndWf.Infrastructure.Extension;
+using PriAndWf.Infrastructure.Helper;
 using System;
 using System.Collections.Generic;
 using System.Net.Http.Headers;
@@ -113,8 +20,16 @@ namespace PriAndWf.TestWebApi.Core
     {
         private const string authenticationScheme = "Digest";
         private const string authenticationRealm = "realm";
-        private const string authenticationQop = "qop";
+        private const string authenticationQop = "qop"; //2个可选值：auth（默认）和auth-int（增加了报文完整性检测）
         private const string authenticationNonce = "nonce";
+        private const string authenticationUsername = "username";
+        private const string authenticationUri = "uri";
+        private const string authenticationNc = "nc";
+        private const string authenticationConnce = "connce";
+        private const string authenticationResponse = "response";
+        private const string authenticationNextnonce = "nextnonce";
+        private const string authenticationRspauth = "rspauth";
+        private const string authenticationStale = "stale";
 
         private ILog logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private UserApplicationService userApplicationService;
@@ -140,7 +55,77 @@ namespace PriAndWf.TestWebApi.Core
                     {
                         //WWW-Authenticate:Digest username="【loginName】", realm="【realm】", nonce="【nonce】", uri="【uri】", cnonce="【cnonce】", nc="【nc】", response="【response】", qop="【qop】"
                         logger.Info(authorization.Parameter + Environment.NewLine);
-                        var authorizationParameterArray = authorization.Parameter.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+                        var authorizationParameterArray = authorization.Parameter.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                        var dic = new Dictionary<string, string>();
+                        if (authorizationParameterArray.Length == 8)
+                        {
+                            for (int i = 0; i < authorizationParameterArray.Length; i++)
+                            {
+                                var kv=authorizationParameterArray[i].Trim().Split(new string[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+                                if (kv.Length == 2)
+                                {
+                                    var k = kv[0].Trim().Trim(new char[] { '"'});
+                                    var v = kv[1].Trim().Trim(new char[] { '"' });
+                                    if (k == authenticationRealm)
+                                    {
+                                        dic.Add(authenticationRealm, v);
+                                    }
+                                    else if (k == authenticationQop)
+                                    {
+                                        dic.Add(authenticationQop, v);
+                                    }
+                                    else if (k == authenticationNonce)
+                                    {
+                                        dic.Add(authenticationNonce, v);
+                                    }
+                                    else if (k == authenticationUsername)
+                                    {
+                                        dic.Add(authenticationUsername, v);
+                                    }
+                                    else if (k == authenticationUri)
+                                    {
+                                        dic.Add(authenticationUri, v);
+                                    }
+                                    else if (k == authenticationNc)
+                                    {
+                                        dic.Add(authenticationNc, v);
+                                    }
+                                    else if (k == authenticationConnce)
+                                    {
+                                        dic.Add(authenticationConnce, v);
+                                    }
+                                    else if (k == authenticationResponse)
+                                    {
+                                        dic.Add(authenticationResponse, v);
+                                    }
+                                }
+                            }
+                            if (dic[authenticationRealm] != null && dic[authenticationQop] != null && dic[authenticationNonce] != null && dic[authenticationUsername] != null
+                            && dic[authenticationUri] != null && dic[authenticationNc] != null && dic[authenticationConnce] != null && dic[authenticationResponse] != null)
+                            {
+                                var user = userApplicationService.GetByLoginName(dic[authenticationUsername]);
+                                if (user != null)
+                                {
+                                    string a1, a2;
+                                    if (dic[authenticationQop].Equals("auth"))
+                                    {
+                                        a1 = $"{dic[authenticationUsername]}:{dic[authenticationRealm]}:{user.Password}";
+                                        a2 = $"{request.Method.Method}:{dic[authenticationUri]}";
+                                    }
+                                    else if (dic[authenticationQop].Equals("auth-int"))
+                                    {
+                                        a1 = $"{RsaHelper.GetStringDigest($"{dic[authenticationUsername]}:{dic[authenticationRealm]}:{user.Password}")}:{dic[authenticationNonce]}:{dic[authenticationConnce]}";
+                                        a2 = $"{request.Method.Method}:{dic[authenticationUri]}:{RsaHelper.GetStringDigest(request.Content.ReadAsStreamAsync().Result)}";
+                                    }
+                                    
+                                    var response = RsaHelper.GetStringDigest()}:{}");
+                                    if (  user.Password.Equals(password))
+                                    var principal = new GenericPrincipal(new GenericIdentity(user.UserName, authenticationScheme), null);
+                                    context.Principal = principal;
+                                    Thread.CurrentPrincipal = principal;
+                                }
+                            }
+                        }
                         ////"Digest 【Base64(loginName:password)】"（不包含【】）
                         //var loginNameAndPasswordStr = Encoding.Default.GetString(Convert.FromBase64String(authorization.Parameter));
                         //var loginNameAndPasswordArray = loginNameAndPasswordStr.Split(new char[] { ':' }, 2, StringSplitOptions.RemoveEmptyEntries);
@@ -174,7 +159,7 @@ namespace PriAndWf.TestWebApi.Core
                     var challenges = new List<AuthenticationHeaderValue>();
                     var authorizationParameterArray = new List<string>();
                     authorizationParameterArray.Add($"{authenticationRealm}={context.Request.RequestUri.DnsSafeHost}");
-                    authorizationParameterArray.Add($"{authenticationQop}=auth");//2个可选值：auth（默认）和auth-int（增加了报文完整性检测）
+                    authorizationParameterArray.Add($"{authenticationQop}=auth");
                     authorizationParameterArray.Add($"{authenticationNonce}={DateTime.Now.Ticks}");
                     challenges.Add(new AuthenticationHeaderValue(authenticationScheme, string.Join(", ", authorizationParameterArray)));
                     context.Result = new UnauthorizedResult(challenges, context.Request);
---
+++ 4.InfrastructureLayer/PriAndWf.Infrastructure/Helper/RedisHelper.cs
@@ -0,0 +1,203 @@
+﻿using PriAndWf.Infrastructure.Extension;
+using StackExchange.Redis;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PriAndWf.Infrastructure.Helper
+{
+    /// <summary>
+    /// ConnectionMultiplexer 对象管理类
+    /// </summary>
+    public static class ConnectionMultiplexerManager
+    {
+        private static ConnectionMultiplexer GetConnection(string configuration, TextWriter log = null)
+        {
+            var connectionMultiplexer = ConnectionMultiplexer.Connect(configuration, log);
+
+            //注册事件
+            connectionMultiplexer.ConfigurationChanged += ConfigurationChanged;
+            connectionMultiplexer.ConfigurationChangedBroadcast += ConfigurationChangedBroadcast;
+            connectionMultiplexer.ConnectionFailed += ConnectionFailed;
+            connectionMultiplexer.ConnectionRestored += ConnectionRestored;
+            connectionMultiplexer.ErrorMessage += ErrorMessage;
+            connectionMultiplexer.HashSlotMoved += HashSlotMoved;
+            connectionMultiplexer.InternalError += InternalError;
+
+            return connectionMultiplexer;
+        }
+
+        #region GetConnectionMultiplexer
+        private static readonly Dictionary<string, ConnectionMultiplexer> concurrentDictionary = new Dictionary<string, ConnectionMultiplexer>();
+        private static readonly object lockerGetConnectionMultiplexer = new object();
+        /// <summary>
+        /// 获取 ConnectionMultiplexer（从缓存中获取，没有时创建）
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
+
+        public static ConnectionMultiplexer GetConnectionMultiplexer(string connectionString, TextWriter log = null)
+        {
+            lock (lockerGetConnectionMultiplexer)
+            {
+                if (!concurrentDictionary.ContainsKey(connectionString))
+                {
+                    concurrentDictionary[connectionString] = GetConnection(connectionString, log);
+                }
+                return concurrentDictionary[connectionString];
+            }
+        }
+        #endregion
+
+        #region 事件
+        private static void ConfigurationChanged(object sender, EndPointEventArgs e)
+        {
+        }
+        private static void ConfigurationChangedBroadcast(object sender, EndPointEventArgs e)
+        {
+        }
+
+        private static void ConnectionFailed(object sender, ConnectionFailedEventArgs e)
+        {
+        }
+        private static void ConnectionRestored(object sender, ConnectionFailedEventArgs e)
+        {
+        }
+
+        private static void ErrorMessage(object sender, RedisErrorEventArgs e)
+        {
+        }
+
+        private static void HashSlotMoved(object sender, HashSlotMovedEventArgs e)
+        {
+        }
+
+        private static void InternalError(object sender, InternalErrorEventArgs e)
+        {
+        }
+        #endregion
+    }
+    public class RedisHelper
+    {
+        private readonly ConnectionMultiplexer _connection;
+        private IDatabase _db;
+
+        public RedisHelper(int db = -1) : this(ConfigurationManager.ConnectionStrings["RedisDefaultConnectionString"].ConnectionString, db)
+        {
+        }
+        public RedisHelper(string connectionString, int db = -1)
+        {
+            _connection = ConnectionMultiplexerManager.GetConnectionMultiplexer(connectionString);
+            _db = _connection.GetDatabase(db);
+        }
+
+        #region IDatabase
+        public IDatabase GetDatabase(int db, object asyncState = null)
+        {
+            return _connection.GetDatabase(db, asyncState);
+        }
+        #endregion
+
+        #region KeyDelete
+        public bool KeyDelete(string key)
+        {
+            return _db.KeyDelete(key);
+        }
+        #endregion
+
+        #region String（字符串）
+        public bool StringSet(string key, string value, TimeSpan? expiry = null)
+        {
+            return _db.StringSet(key, value, expiry);
+        }
+        public RedisValue StringGet(string key)
+        {
+            return _db.StringGet(key);
+        }
+        #endregion
+
+        #region Hash（哈希）
+        //public void HashSet(string key, string hashField, object value)
+        //{
+        //    value
+        //    _db.HashSet(key, hashField, value);
+        //}
+        //public void HashSet(string key, string hashField, byte[] value)
+        //{
+        //    _db.HashSet(key, hashField, value);
+        //}
+        //public void HashSet(string key, string hashField, string value, TimeSpan expiry)
+        //{
+        //    _db.HashSet(key, hashField, value);
+        //    _db.KeyExpire(key, expiry);
+        //}
+        //public void HashSet(RedisKey key, RedisValue hashField, RedisValue value, DateTime expiry)
+        //{
+        //    _db.HashSet(key, hashField, value);
+        //    _db.KeyExpire(key, expiry);
+        //}
+        public RedisValue HashGet(RedisKey key, RedisValue hashField)
+        {
+            return _db.HashGet(key, hashField);
+        }
+        public HashEntry[] HashGet(RedisKey key)
+        {
+            return _db.HashGetAll(key);
+        }
+        public bool HashDelete(RedisKey key, RedisValue hashField)
+        {
+            return _db.HashDelete(key, hashField);
+        }
+        #endregion
+
+        #region List（列表）
+
+        #endregion
+
+        #region Set（集合）
+
+        #endregion
+
+        #region Sorted Set（有序集合）
+
+        #endregion
+
+        #region 序列化、反序列化（二进制序列化、JSON序列化、XML序列化...）
+        /// <summary>
+        /// 二进制序列化
+        /// </summary>
+        /// <param name="obj">待二进制序列化的数据</param>
+        /// <returns>二进制序列化后的字节数组</returns>
+        private byte[] BinarySerialize(object obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+        /// <summary>
+        /// 二进制反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="serializedObject">二进制序列化后的字节数组</param>
+        /// <returns></returns>
+        private T Deserialize<T>(byte[] serializedObj)
+        {
+            using (var ms = new MemoryStream(serializedObj))
+            {
+                var obj = new BinaryFormatter().Deserialize(ms);
+                return (T)obj;
+            }
+        }
+        #endregion
+    }
+}
